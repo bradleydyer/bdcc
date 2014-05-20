860705668cd732bf20236b3a8d156703d2f25e88
krisrybak <kris.rybak@hotmail.co.uk>
2014-05-20 09:59:17 +0100

    Adds base for Bdcc_Http_Client

---
+++ Bdcc/Http/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bdcc\Http;
+
+/**
+ * Bdcc_Http_Client Class
+ *
+ * Provides methods to communicate with HTTP servers
+ * @author Kris Rybak kris.rybak@bradleydyer.com
+ */
+class Client
+{
+
+}
