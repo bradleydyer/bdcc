76f55252c31ca3cb13cbc0ec05699878b2e1dddd
krisrybak <kris.rybak@hotmail.co.uk>
2014-05-29 12:22:18 +0100

    Sets new class for Bdcc_Json_Parser

---
+++ Bdcc/Json/Parser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bdcc\Json;
+
+/**
+ * Bdcc_Json_Parser Class
+ *
+ * Allows decoding and encoding of JSON
+ * @author Kris Rybak kris.rybak@bradleydyer.com
+ */
+class Parser
+{
+    
+}
