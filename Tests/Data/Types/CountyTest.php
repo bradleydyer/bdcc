2a37a0dc4e40d6bf1e28b4e853bc92327a7bfcf4
McAnton <asmccook@gmail.com>
2014-08-01 12:30:44 +0100

    Fixed assoc output and updated test

--- Tests/Data/Types/CountyTest.php
+++ Tests/Data/Types/CountyTest.php
@@ -37,4 +37,21 @@ class CountyTest extends TestCase
             $this->assertTrue($key === $value);
         }
     }
+
+    public function testWithCountriesAssoc()
+    {
+        $counties = County::getCounties(true,true);
+        $this->assertTrue(!empty($counties));
+        $this->assertTrue(is_array($counties));
+        foreach ($counties as $countryName => $countryCounties) {
+            // Check the key (Country name) is a string, and the value (Counties) is array
+            $this->assertTrue(is_string($countryName));
+            $this->assertTrue(is_array($countryCounties));
+            foreach ($countryCounties as $key => $value) {
+                // Check the key (County name) is a string, and the key and value (County name) are the same
+                $this->assertTrue(is_string($key));
+                $this->assertTrue($key === $value);
+            }
+        }
+    }
 }
