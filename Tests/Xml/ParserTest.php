fa395e50ba8ce7aed7827e40656f989dc1573c15
McAnton <asmccook@gmail.com>
2015-03-11 15:44:40 +0000

    Added more tests

--- Tests/Xml/ParserTest.php
+++ Tests/Xml/ParserTest.php
@@ -16,6 +16,25 @@ class ParserTest extends TestCase
 {
     private $data;
 
+    private $dataValidation = array(
+        0 => array(
+            'name'  => 'to',
+            'value' => 'BradleyDyer',
+        ),
+        1 => array(
+            'name'  => 'from',
+            'value' => 'Developer',
+        ),
+        2 => array(
+            'name'  => 'heading',
+            'value' => 'Sample XML File',
+        ),
+        3 => array(
+            'name'  => 'body',
+            'value' => 'This is a sample XML file',
+        ),
+    );
+
     public function setUp()
     {
         // Create the path to the sample.xml file
@@ -39,6 +58,20 @@ class ParserTest extends TestCase
         $this->data = fread($handle, filesize($file));
     }
 
+    public function testGetAndSetXmlReader()
+    {
+        // Test by default no reader has been created
+        $actual = Parser::getXmlReader();
+        $this->assertTrue(is_null($actual));
+
+        // Set a new XmlReader and test we get the same object.
+        $expected = new \XmlReader();
+        Parser::setXmlReader($expected);
+        $actual = Parser::getXmlReader();
+
+        $this->assertSame($expected, $actual);
+    }
+
     public function testXmlParse()
     {
         // Parse the xml string
@@ -47,6 +80,24 @@ class ParserTest extends TestCase
         // Test the type of class returned is a StdClass
         $expected = '\\StdClass';
         $this->assertInstanceOf($expected, $actual);
+
+        // Test the root element has the name note
+        $this->assertSame("note", $actual->name);
+
+        // Test the root element value is of type array
+        $this->assertTrue(is_array($actual->value));
+
+        // Get the root elements children
+        $children = $actual->value;
+
+        // Test there is the correct number of child elements
+        $this->assertTrue(count($children) == 4);
+
+        // Test the data of the root elements children
+        for($i=0; $i < count($children); $i++) {
+            $this->assertSame($this->dataValidation[$i]['name'], $children[$i]->name);
+            $this->assertSame($this->dataValidation[$i]['value'], $children[$i]->value);
+        }
 
         // Test invalid argument
         $this->setExpectedException('\\InvalidArgumentException');
